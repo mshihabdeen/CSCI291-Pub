b3438ca723feb36d72d46c34354b13fca6d8d963
Muaadh Shihabdeen <55185634+mshihabdeen@users.noreply.github.com>
2024-11-17 23:03:44 +0400

    Add files via upload

--- Lab3_Q3.c
+++ Lab3_Q3.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define SQUAD_SIZE 11
+#define NUM_TEAMS 8
 
 struct age
 {
@@ -23,4 +26,83 @@ struct team_t{
     struct player_t players[SQUAD_SIZE];
     int activeSize;
 };
+
+//Variables
+
+int teamcount = 0;
+struct team_t Teams[NUM_TEAMS];
+
+//Prototypes
+void menu();
+int enrollclub();
+void addplayer();
+void searchupdate();
+void displaystats();
+
+
+
+//main
+int main() {
+    menu();
+}
+
+
+//Functions
+void menu(){
+    while(1){
+        int i = 0;
+
+        printf("\nMain Menu\n1. Enroll Club\n2. Add Player\n3. Search and Update Player Details\n4. Display Club Stats\n5. Exit\n\n");
+        scanf("%d",&i);
+
+        switch (i) {
+            case 1:
+                enrollclub();
+                break;
+            case 2:
+                addplayer();
+                break;
+            case 3:
+                //searchupdate();
+                break;
+            case 4:
+                //displaystats();
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                printf("Invalid Selection. Try Again");
+
+        }
+
+    }
+}
+
+int enrollclub(){
+    if ((teamcount) >= NUM_TEAMS-1){
+        printf("Cannot Enroll Team; No Vacancies\n");
+        return 0;
+    }
+    else{
+        printf("Enter club name: ");
+        fgets(Teams[teamcount].name, 20, stdin);
+        Teams[teamcount].name[strcspn(Teams[teamcount].name, "\n")] = '\0';
+        Teams[teamcount].activeSize = 0;
+        printf("Club enrolled successfully.\n");
+        teamcount++;
+        return 0;
+    }
+}
+
+void addplayer(){
+    int clubid;
+    printf("Select Club ID (0-%d): ", teamcount);
+    for (int i=0;i<NUM_TEAMS;i++){
+        printf("\n%s",Teams[i].name);
+    }
+    scanf("%d", &clubid);
+
+}
+
 
